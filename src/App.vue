8f20f59d7cedca72c3f29ec6ad2bc7da715e01f9
e <570834681@qq.com>
2020-03-16 17:29:24 +0800

    vue-swiper-template

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <swiper>
+      <swiper-item>
+        <img alt="" src="http://oss-image3.quanmamaimg.com/AdminImageUpload/2019/8/22/2439945wap_app0yg_ORIGIN_xkzN.png" />
+      </swiper-item>
+      <swiper-item>
+        <img alt="" src="http://oss-image3.quanmamaimg.com/AdminImageUpload/2019/8/22/2439945wap_app0yg_ORIGIN_xkzN.png" />
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import {Swiper,SwiperItem} from './components/vueSwiper'
+
+export default {
+  name: 'App',
+  components: {
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/vueSwiper/SwiperItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="swiper-slide">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+//https://www.cnblogs.com/makai/p/9002898.html
+export default {
+  name: '',
+  data() { 
+    return {
+    }
+  }
+ }
+</script>
